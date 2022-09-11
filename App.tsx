da59dcf2339e0e96be2ab25b17c02baf2f58b13e
pikokr <parangee9706@gmail.com>
2022-09-11 11:13:32 +0900

    feat: loading screen

--- App.tsx
+++ App.tsx
@@ -9,9 +9,15 @@
  */
 
 import RNRestart from 'react-native-restart'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
-import { BackHandler, useColorScheme, View } from 'react-native'
+import {
+  ActivityIndicator,
+  BackHandler,
+  useColorScheme,
+  View,
+  Animated,
+} from 'react-native'
 import axios from 'axios'
 import { Banner, Gaknime } from './types'
 import { StyledText } from './components/Text'
@@ -88,67 +94,125 @@ const App = () => {
     }
   }, [isDark])
 
-  return loading ? null : error ? (
-    <View
-      style={{
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100%',
-        flexDirection: 'column',
-      }}
-    >
-      <StyledText style={{ fontSize: 24 }} weight="Bold">
-        앗! 오류가 발생했어요!
-      </StyledText>
-      <StyledText style={{ fontSize: 16 }} weight="Light">
-        {error.message}
-      </StyledText>
-      <View style={{ display: 'flex', flexDirection: 'row', marginTop: 12 }}>
-        <Button onClick={restartApp}>
-          <StyledText style={{ color: '#fff' }}>재시작하기</StyledText>
-        </Button>
-        <Button onClick={exitApp} style={{ marginLeft: 12 }}>
-          <StyledText style={{ color: '#fff' }}>나가기</StyledText>
-        </Button>
-      </View>
-    </View>
-  ) : (
-    <View style={{ backgroundColor: theme.background, height: '100%' }}>
-      <RecoilRoot>
-        <ThemeContext.Provider value={theme}>
-          <GaknimesContext.Provider value={gaknimes}>
-            <BannersContext.Provider value={banners}>
-              <NavigationContainer
-                theme={{
-                  dark: isDark,
-                  colors: {
-                    background: theme.background,
-                    card: theme.background,
-                    text: theme.text,
-                    border: theme.footerBorder,
-                    primary: theme.primary,
-                    notification: theme.primary,
-                  },
-                }}
-              >
-                <Stack.Navigator>
-                  <Stack.Screen
-                    options={{ headerShown: false }}
-                    name="Home"
-                    component={Main}
-                  />
-                  <Stack.Screen
-                    options={{ headerShown: false }}
-                    name="Search"
-                    component={SearchPage}
-                  />
-                </Stack.Navigator>
-              </NavigationContainer>
-            </BannersContext.Provider>
-          </GaknimesContext.Provider>
-        </ThemeContext.Provider>
-      </RecoilRoot>
-    </View>
+  const loadingOpacity = useRef(new Animated.Value(1)).current
+  const contentOpacity = useRef(new Animated.Value(0)).current
+
+  const [loadingDone, setLoadingDone] = React.useState(false)
+
+  useEffect(() => {
+    if (!loading) {
+      Animated.timing(loadingOpacity, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: false,
+      }).start()
+
+      Animated.timing(contentOpacity, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: false,
+        delay: 500,
+      }).start()
+
+      setTimeout(() => {
+        setLoadingDone(true)
+      }, 1000)
+    }
+  }, [contentOpacity, loading, loadingOpacity])
+
+  return (
+    <ThemeContext.Provider value={theme}>
+      <Animated.View
+        style={[
+          {
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%',
+            display: loadingDone ? 'none' : 'flex',
+            position: 'absolute',
+            left: 0,
+            top: 0,
+            width: '100%',
+          },
+          { opacity: loadingOpacity },
+        ]}
+      >
+        <StyledText weight="Black" style={{ fontSize: 36, lineHeight: 40 }}>
+          GAKNIME
+        </StyledText>
+        <View style={{ marginTop: 12 }}>
+          <ActivityIndicator size="large" color={theme.text} />
+        </View>
+      </Animated.View>
+
+      <Animated.View
+        style={{
+          opacity: contentOpacity,
+          height: '100%',
+        }}
+      >
+        {loading ? null : error ? (
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100%',
+              flexDirection: 'column',
+            }}
+          >
+            <StyledText style={{ fontSize: 24 }} weight="Bold">
+              앗! 오류가 발생했어요!
+            </StyledText>
+            <StyledText style={{ fontSize: 16 }} weight="Light">
+              {error.message}
+            </StyledText>
+            <View
+              style={{ display: 'flex', flexDirection: 'row', marginTop: 12 }}
+            >
+              <Button onClick={restartApp}>
+                <StyledText style={{ color: '#fff' }}>재시작하기</StyledText>
+              </Button>
+              <Button onClick={exitApp} style={{ marginLeft: 12 }}>
+                <StyledText style={{ color: '#fff' }}>나가기</StyledText>
+              </Button>
+            </View>
+          </View>
+        ) : (
+          <RecoilRoot>
+            <GaknimesContext.Provider value={gaknimes}>
+              <BannersContext.Provider value={banners}>
+                <NavigationContainer
+                  theme={{
+                    dark: isDark,
+                    colors: {
+                      background: theme.background,
+                      card: theme.background,
+                      text: theme.text,
+                      border: theme.footerBorder,
+                      primary: theme.primary,
+                      notification: theme.primary,
+                    },
+                  }}
+                >
+                  <Stack.Navigator>
+                    <Stack.Screen
+                      options={{ headerShown: false }}
+                      name="Home"
+                      component={Main}
+                    />
+                    <Stack.Screen
+                      options={{ headerShown: false }}
+                      name="Search"
+                      component={SearchPage}
+                    />
+                  </Stack.Navigator>
+                </NavigationContainer>
+              </BannersContext.Provider>
+            </GaknimesContext.Provider>
+          </RecoilRoot>
+        )}
+      </Animated.View>
+    </ThemeContext.Provider>
   )
 }
 
